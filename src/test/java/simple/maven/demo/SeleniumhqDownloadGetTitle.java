ba940841f1aefb8de38e1effceef3fc9700f0935
PavithraTYSS26 <pavithraoptimist@gmail.com>
2019-05-15 18:03:35 +0530

    may month maven git jenkins

--- src/test/java/simple/maven/demo/SeleniumhqDownloadGetTitle.java
+++ src/test/java/simple/maven/demo/SeleniumhqDownloadGetTitle.java
@@ -9,22 +9,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumhqDownloadGetTitle {
-@Test
-public void getTitlel()
-{
-	System.setProperty("webdriver.chrome.driver", "D:\\TestYantra\\SrinidhiSimpleMavenProject\\src\\test\\resources\\softwares\\chromedriver.exe");
-	WebDriver driver = new ChromeDriver();
-	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	@Test
+	public void getTitlel() {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\TestYantra\\SrinidhiSimpleJenkinsMavenProject\\Softwares\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-	
-	driver.get("https://www.seleniumhq.org");
-	driver.manage().window().maximize();
-	
-	//	System.out.println("Selenium");
-	///String sel=driver.findElement(By.xpath(""));
-	 System.out.println(driver.getTitle());
-	driver.findElement(By.xpath("//a[text()='Download']")).click();
-	 System.out.println(driver.getTitle());
-    driver.close();
-}
+		driver.get("https://www.seleniumhq.org");
+		driver.manage().window().maximize();
+
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//a[text()='Download']")).click();
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
 }
